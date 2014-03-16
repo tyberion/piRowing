2a91087bba044f33987410c7956ae8f73c2234b8
Maarten Kruithof <mckruithuijb@gmail.com>
2014-03-16 19:49:36 +0000

    adde websocket example

---
+++ websocket_tornado_redis.py
@@ -0,0 +1,59 @@
+import threading
+
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import tornado.template
+
+import redis
+
+import os.path
+
+SERVER = '192.168.178.60'
+PORT = 8888
+
+LOADER = tornado.template.Loader('templates').load('index.html')
+TEMPLATE =  LOADER.generate(server=SERVER, port='%d' % PORT)
+
+LISTENERS = []
+
+def redis_listener():
+    r = redis.Redis(host='localhost', db=2)
+    ps = r.pubsub()
+    ps.subscribe('test_realtime')
+    for message in ps.listen():
+        for element in LISTENERS:
+            element.write_message(message['data'])
+
+
+class NewMsgHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html', server=SERVER, port='%d' % PORT)
+
+class RealtimeHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        LISTENERS.append(self)
+
+    def on_message(self, message):
+        pass
+
+    def on_close(self):
+        LISTENERS.remove(self)
+
+settings = dict(
+    auto_reload = True,
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+)
+
+application = tornado.web.Application([
+    (r'/', NewMsgHandler),
+    (r'/realtime/', RealtimeHandler),
+], **settings)
+
+if __name__ == "__main__":
+    threading.Thread(target=redis_listener).start()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(PORT)
+    tornado.ioloop.IOLoop.instance().start()
