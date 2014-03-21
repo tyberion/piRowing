eff6716f59c04231ee657663a4c97aac2e86f154
Maarten Kruithof <mckruithuijb@gmail.com>
2014-03-21 09:04:19 +0000

    updates

--- send_message.py
+++ send_message.py
@@ -1,10 +1,25 @@
 import redis
 import time
 
+import numpy as np
+
 r = redis.Redis(host='localhost', db=2)
 
-for i in range(100):
-    r.publish('test_realtime',dict(msg01='%d' % i, msg02='%d' % (i*2)))
-    time.sleep(0.1)
+r.publish('test_realtime',[])
+time.sleep(0.1)
 
+N = 1000;
 
+xx = np.linspace(0,2*np.pi,N)
+yy = np.sin(xx)
+yy += (np.random.rand(N)-0.5)/2
+
+mesg = []
+
+for idx,(x,y) in enumerate(zip(xx, yy)):
+    mesg.append([x,y])
+    if idx % 1 == 0:
+        r.publish('test_realtime',mesg)
+        time.sleep(0.2)
+        mesg = []
+r.publish('test_realtime',mesg)
